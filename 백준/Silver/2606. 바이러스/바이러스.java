198b5033062e4b4a1fc07f0560bcf9b3afca9349
개발민수 <minsoo010@naver.com>
2025-09-22 19:47:10 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14280 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int count = 0;
+
+    static void dfs(int node, List<Integer>[] graph, boolean[] visited) {
+        visited[node] = true;
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                count++;
+                dfs(next, graph, visited);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        List<Integer>[] graph = new ArrayList[n+1];
+        boolean[] visited = new boolean[n+1];
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        dfs(1, graph, visited);
+        System.out.println(count);
+    }
+}
