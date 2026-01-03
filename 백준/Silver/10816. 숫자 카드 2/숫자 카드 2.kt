de6e3eb4aeb15057918a116bb1f30deae975bed4
개발민수 <minsoo010@naver.com>
2026-01-03 23:21:31 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1264 ms, Memory: 254436 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.kt
@@ -5,14 +5,15 @@ fun main() {
     val n = readln().toInt()
     val cards = readln().split(" ").map { it.toInt() }
 
-    val countMap = mutableMapOf<Int, Int>()
-    for (card in cards) {
-        countMap[card] = countMap.getOrDefault(card, 0) + 1
-    }
+    val cardCountMap = hashMapOf<Int, Int>()
+    cards.forEach { cardCountMap[it] = cardCountMap.getOrDefault(it, 0) + 1 }
 
     val m = readln().toInt()
-    val queries = readln().split(" ").map { it.toInt() }
+    val numbers = readln().split(" ").map { it.toInt() }
 
-    val result = queries.map { countMap.getOrDefault(it, 0) }
-    println(result.joinToString(" "))
+    println(
+        numbers.joinToString(" ") {
+            cardCountMap.getOrDefault(it, 0).toString()
+        }
+    )
 }
