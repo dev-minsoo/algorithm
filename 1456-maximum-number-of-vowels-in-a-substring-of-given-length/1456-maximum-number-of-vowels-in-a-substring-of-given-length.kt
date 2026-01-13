b1684ac2b57b27062c971e5e2240dc6af2e24e19
개발민수 <minsoo010@naver.com>
2026-01-13 19:55:27 +0900

    Time: 56 ms (12.98%), Space: 49.2 MB (10.58%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.kt
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.kt
@@ -1,17 +1,16 @@
 class Solution {
     fun maxVowels(s: String, k: Int): Int {
-        val vowels = setOf('a', 'e', 'i', 'o', 'u')
-        
-        var currentCount = s.take(k).count { it in vowels }
+        val set = setOf('a', 'e', 'i', 'o', 'u')
+        var currentCount = s.take(k).count { it in set }
         var maxCount = currentCount
 
         for (i in k until s.length) {
-            if (s[i-k] in vowels) currentCount--
-            if (s[i] in vowels) currentCount++
+            if (s[i-k] in set) currentCount--
+            if (s[i] in set) currentCount++
 
             maxCount = maxOf(maxCount, currentCount)
         }
-
+        
         return maxCount
     }
 }
