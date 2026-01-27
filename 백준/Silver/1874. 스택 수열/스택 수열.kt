9a2b3a8dd951afab920bb2be3e9f32c76b88b3d6
개발민수 <minsoo010@naver.com>
2026-01-27 20:45:07 +0900

    [Silver II] Title: 스택 수열, Time: 328 ms, Memory: 32192 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.kt
+++ 백준/Silver/1874. 스택 수열/스택 수열.kt
@@ -1,34 +1,28 @@
-import java.util.Stack
-
+/**
+ * p1874 스택 수열
+ */
 fun main() {
     val n = readln().toInt()
-    val stack = Stack<Int>()
+    val stack = ArrayDeque<Int>()
     val sb = StringBuilder()
-
-    var current = 1
+    var cur = 1
     var possible = true
 
     repeat(n) {
-        val target = readln().toInt()
+        val number = readln().toInt()
 
-        while (current <= target) {
-            stack.push(current++)
-            sb.appendLine("+")
+        while (cur <= number) {
+            stack.addLast(cur++)
+            sb.append("+\n")
         }
 
-        if (stack.peek() == target) {
-            stack.pop()
-            sb.appendLine("-")
+        if (stack.last() == number) {
+            stack.removeLast()
+            sb.append("-\n")
         } else {
             possible = false
-            return@repeat
         }
     }
 
-    if (possible) {
-        println(sb)
-    } else {
-        println("NO")
-    }
-
+    println(if (possible) sb else "NO")
 }
