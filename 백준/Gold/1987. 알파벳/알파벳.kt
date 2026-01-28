235c7900f1102c861fb6c36465760af57da8851a
개발민수 <minsoo010@naver.com>
2026-01-28 20:30:38 +0900

    [Gold IV] Title: 알파벳, Time: 2408 ms, Memory: 300504 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.kt
+++ 백준/Gold/1987. 알파벳/알파벳.kt
@@ -1,19 +1,18 @@
 /**
  * p1987 알파벳
  */
-fun dfs(x: Int, y: Int, count: Int, grid: Array<CharArray>, visited: BooleanArray): Int {
+private fun dfs(x: Int, y: Int, count: Int, graph: Array<CharArray>, visited: BooleanArray): Int {
     var maxCount = count
-
     val dirs = arrayOf(1 to 0, -1 to 0, 0 to 1, 0 to -1)
 
-    for ((dx, dy) in dirs) {
-        val nx = x + dx
-        val ny = y + dy
-        if (nx in 0 until grid.size && ny in 0 until grid[0].size) {
-            val idx = grid[nx][ny] - 'A'
+    for (dir in dirs) {
+        val nx = x + dir.first
+        val ny = y + dir.second
+        if (nx in graph.indices && ny in graph[0].indices) {
+            val idx = graph[nx][ny] - 'A'
             if (!visited[idx]) {
                 visited[idx] = true
-                maxCount = maxOf(maxCount, dfs(nx, ny, count+1, grid, visited))
+                maxCount = maxOf(maxCount, dfs(nx, ny, count + 1, graph, visited))
                 visited[idx] = false
             }
         }
@@ -24,10 +23,11 @@ fun dfs(x: Int, y: Int, count: Int, grid: Array<CharArray>, visited: BooleanArra
 
 fun main() {
     val (r, c) = readln().split(" ").map { it.toInt() }
-    val grid = Array(r) { readln().toCharArray() }
+    val graph = Array(r) { readln().toCharArray() }
     val visited = BooleanArray(26)
 
-    visited[grid[0][0] - 'A'] = true
-    val maxCount = dfs(0, 0, 1, grid, visited)
-    println(maxCount)
+    visited[graph[0][0] - 'A'] = true
+    val result = dfs(0, 0, 1, graph, visited)
+
+    println(result)
 }
